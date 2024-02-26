23fba540680b3be72e40e2e65dc037dee01f026a
nex <me@nexy7574.co.uk>
2024-02-26 14:31:14 +0000

    Improve pypi webhook

--- modules/pypi_releases.py
+++ modules/pypi_releases.py
@@ -37,27 +37,28 @@ class PyPiReleases(niobot.Module):
             }
             if self.last_etag:
                 headers["If-None-Match"] = self.last_etag
-
+            self.log.debug("Requesting RSS @ %s", self.url)
             response = await client.get(self.url, headers=headers)
-            self.log.debug(response)
+            self.log.debug("Response: %r", response)
             if response.status_code == 304:
                 return
             response.raise_for_status()
             self.last_etag = response.headers.get("etag", self.last_etag)
+            self.log.debug("Set etag to %r", self.last_etag)
             root = ElementTree.fromstring(response.content)
             for item in root.findall("./channel/item"):
-                self.log.debug(item)
+                self.log.debug("Found item in ./channel/item: %r", item)
                 title = item.find("title").text
-                self.log.debug("title: %r", title)
+                self.log.debug("Title: %r", title)
                 link = item.find("link").text
                 self.log.debug("Link: %s", link)
                 pub_date = item.find("pubDate").text
                 parsed = datetime.datetime.strptime(pub_date, "%a, %d %b %Y %H:%M:%S %Z")
                 self.log.debug("Date: %s (%r)", pub_date, parsed)
                 if parsed < self.started_at:
-                    self.log.debug("Old.")
+                    self.log.debug("Entry is old(er than %s)", self.started_at.isoformat())
                     continue
-                self.log.debug("Ok")
+                self.log.debug("Entry is new(er than %s), sending message", self.started_at.isoformat())
 
                 await self.bot.send_message(
                     self.room_id,
